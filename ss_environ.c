e78e70bad6c19e2297a91a47eedcacd7e1cd29a2
Hammedadebayo <hammedadebayo220@gmail.com>
2023-05-24 06:57:08 -0700

    commiting the environment and library functions

---
+++ ss_environ.c
@@ -0,0 +1,103 @@
+#include "shell.h"
+
+/**
+ * _grpfnsenv - function that prints the current environment
+ * @info: parameter for structure.
+ * Return: Always 0
+ */
+int _grpfnsenv(info_t *info)
+{
+print_list_str(info->env);
+return (0);
+}
+
+/**
+ * _getenv - function that gets the value of an environ variable
+ * @info: parameter for structure.
+ * @name: parameter for env var
+ * Return: the value
+ */
+char *_getenv(info_t *info, const char *name)
+{
+/*introducing parameter for function*/
+char *ptr;
+list_t *node = info->env;
+
+/*introducing conditional statement*/
+while (node)
+{
+ptr = starts_with(node->str, name);
+if (ptr && *ptr)
+return (ptr);
+node = node->next;
+}
+return (NULL);
+}
+
+/**
+ * _grpfnssetenv - function that Initialize a new environment variable,
+ * @info: parameter for structure.
+ * Return: Always 0
+ */
+int _grpfnssetenv(info_t *info)
+{
+/*introducing conditional statement*/
+if (info->argc != 3)
+{
+_eputs("Incorrect number of arguements\n");
+return (1);
+}
+if (_setenv(info, info->argv[1], info->argv[2]))
+return (0);
+return (1);
+}
+
+/**
+ * _grpfnsunsetenv - function that Remove an environment variable
+ * @info: parameter for structure.
+ * Return: Always 0
+ */
+int _grpfnsunsetenv(info_t *info)
+{
+int i;
+
+if (info->argc == 1)
+{
+_eputs("arguements are few.\n");
+return (1);
+}
+for (i = 1; i <= info->argc; i++)
+_unsetenv(info, info->argv[i]);
+
+return (0);
+}
+
+/**
+ * _grpfnsexit - function that exits the shell
+ * @info: parameter for structure.
+ * Return: parameter and a given exit status
+ * (0) if info.argv[0] != "exit"
+ */
+int _grpfnsexit(info_t *info)
+{
+/*introducing parameter for function*/
+int exitcheck;
+
+/*introducing conditional statement*/
+if (info->argv[1])
+{
+exitcheck = _erratoi(info->argv[1]);
+if (exitcheck == -1)
+{
+info->status = 2;
+print_error(info, "Invalid number: “);
+_eputs(info->argv[1]);
+_eputchar('\n');
+return (1);
+}
+info->err_num = _erratoi(info->argv[1]);
+return (-2);
+}
+info->err_num = -1;
+return (-2);
+}
---
+++ ss_lib.c
@@ -0,0 +1,103 @@
+#include "shell.h"
+
+/**
+ * _eputs - function that prints an input string
+ * @str: parameter for the string to be printed
+* Return: Nothing
+ */
+void _eputs(char *str)
+{
+/*introducing parameter for function*/
+int i = 0;
+
+/*introducing conditional statement*/
+if (!str)
+return;
+while (str[i] != '\0')
+{
+_eputchar(str[i]);
+i++;
+}
+}
+
+/**
+ * _eputchar - function that writes the character c to stderr
+ * @c: parameter for The character to print
+* Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _eputchar(char c)
+{
+static int i;
+static char buf[WRITE_BUF_SIZE];
+
+/*introducing conditional statement*/
+if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+{
+write(2, buf, i);
+i = 0;
+}
+if (c != BUF_FLUSH)
+buf[i++] = c;
+return (1);
+}
+
+/**
+ * _putfd - function that writes the character c to given fd
+ * @c: parameter for The character to print
+ * @fd: parameter for The filedescriptor to write to
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+/*introducing parameter for function*/
+static int i;
+static char buf[WRITE_BUF_SIZE];
+
+if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+{
+write(fd, buf, i);
+i = 0;
+}
+if (c != BUF_FLUSH)
+buf[i++] = c;
+return (1);
+}
+
+/**
+ * _putsfd - function that prints an input string
+ * @str: parameter for the string to be printed
+ * @fd: parameter for the filedescriptor to write to
+ * Return: the number of chars put
+ */
+int _putsfd(char *str, int fd)
+{
+/*introducing parameter for function*/
+int i = 0;
+
+if (!str)
+return (0);
+while (*str)
+{
+i += _putfd(*str++, fd);
+}
+return (i);
+}
+/**
+ * print_error - function that prints an error message
+ * @info: parameter for the parameter & return info struct
+ * @estr: parameter for string containing specified error type
+ * Return: 0 if no numbers in string, converted number otherwise
+ *        -1 on error
+ */
+void print_error(info_t *info, char *estr)
+{
+_eputs(info->val_f);
+_eputs(": ");
+print_d(info->var_ind, STDERR_FILENO);
+_eputs(": ");
+_eputs(info->argv[0]);
+_eputs(": ");
+_eputs(estr);
+}
