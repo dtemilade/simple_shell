2f71b42f39dca398d4417e79e486ce6fbf86d08e
Hammedadebayo <hammedadebayo220@gmail.com>
2023-05-24 17:13:01 -0700

    commiting for the correction of builtins functions

--- ss_builtins.c
+++ ss_builtins.c
@@ -9,19 +9,19 @@
  */
 int unset_alias(info_t *info, char *str)
 {
-/*introducing parameters*/
-char *ptr, c;
-int ret;
+	/*introducing parameters*/
+	char *ptr, c;
+	int ret;
 
-ptr = _strchr(str, '=');
-if (!ptr)
-return (1);
-c = *ptr;
-*ptr = 0;
-ret = deleten_at_index(&(info->alias),
-getn_index(info->alias, node_starts_with(info->alias, str, -1)));
-*ptr = c;
-return (ret);
+	ptr = _strchr(str, '=');
+	if (!ptr)
+		return (1);
+	c = *ptr;
+	*ptr = 0;
+	ret = deleten_at_index(&(info->alias),
+			getn_index(info->alias, node_starts_with(info->alias, str, -1)));
+	*ptr = c;
+	return (ret);
 }
 
 /**
@@ -31,48 +31,48 @@ return (ret);
  */
 int _grpfnscd(info_t *info)
 {
-/*introducing parameters*/
-char *s, *dir, buffer[1024];
-int chdir_ret;
+	/*introducing parameters*/
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
 
-s = getcwd(buffer, 1024);
-/*introducing conditional statement*/
-if (!s)
-_puts("TODO: parameter for>>getcwd failure emsg here<<\n");
-if (!info->argv[1])
-{
-dir = _getenv(info, "HOME=");
-if (!dir)
-chdir_ret =
-chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
-else
-chdir_ret = chdir(dir);
-}
-else if (_strcmp(info->argv[1], "-") == 0)
-{
-if (!_getenv(info, "OLDPWD="))
-{
-_puts(s);
-_putchar('\n');
-return (1);
-}
-_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-chdir_ret =
-chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
-}
-else
-chdir_ret = chdir(info->argv[1]);
-if (chdir_ret == -1)
-{
-print_error(info, "can't cd to ");
-_eputs(info->argv[1]), _eputchar('\n');
-}
-else
-{
-_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-_setenv(info, "PWD", getcwd(buffer, 1024));
-}
-return (0);
+	s = getcwd(buffer, 1024);
+	/*introducing conditional statement*/
+	if (!s)
+		_puts("TODO: parameter for>>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	{
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret =
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret =
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
 }
 
 
@@ -83,7 +83,7 @@ return (0);
  */
 int interactive(info_t *info)
 {
-return (isatty(STDIN_FILENO) && info->readfd <= 2);
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
@@ -94,11 +94,11 @@ return (isatty(STDIN_FILENO) && info->readfd <= 2);
  */
 int is_delim(char c, char *delim)
 {
-/*introducing conditional statement*/
-while (*delim)
-if (*delim++ == c)
-return (1);
-return (0);
+	/*introducing conditional statement*/
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
 }
 
 /**
@@ -109,30 +109,30 @@ return (0);
 
 int _atoi(char *s)
 {
-/*introducing parameter for function*/
-int i, sign = 1, flag = 0, output;
-unsigned int result = 0;
+	/*introducing parameter for function*/
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
 
-/*introducing conditional statement*/
-for (i = 0; s[i] != '\0' && flag != 2; i++)
-{
-if (s[i] == '-')
-sign *= -1;
+	/*introducing conditional statement*/
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
 
-if (s[i] >= '0' && s[i] <= '9')
-{
-flag = 1;
-result *= 10;
-result += (s[i] - function that '0');
-}
-else if (flag == 1)
-flag = 2;
-}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - function that '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
 
-if (sign == -1)
-output = -result;
-else
-output = result;
-
-return (output);
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+	
+	return (output);
 }
