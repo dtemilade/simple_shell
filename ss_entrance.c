778e01ac46e9f0cefd77dc6ed2851bca5534a190
dtemilade <dtemilade@gmail.com>
2023-05-26 04:44:58 -0700

    correction continues on simple_shell source file

--- ss_entrance.c
+++ ss_entrance.c
@@ -26,7 +26,7 @@ int main(int ac, char **av)
 			if (errno == ENOENT)
 			{
 				_eputs(av[0]);
-				_eputs(": parameter for 0: parameter for Can't open ");
+				_eputs(": 0: Can't open ");
 				_eputs(av[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
